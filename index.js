6bb47208170251106350ebeaa3f3a9e2083ea641
Jarrod Thibodeau <thibodeaujz@gmail.com>
2019-04-08 21:33:19 -0400

    le validator bby

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const cors = require('micro-cors')();
 const parse = require('urlencoded-body-parser');
 const { post } = require('axios');
-const redirect = require('micro-redirect')
+const redirect = require('micro-redirect');
+const validator = require('validator');
 
 const handler = async (req, res) => {
-    const data = await parse(req);
+    const {
+        name,
+        email,
+        redirect_to,
+        form_destination,
+        message
+    } = await parse(req);
 
-    let formIsValid = true;
-    console.log('the body: ', data);
+    const formIsValid = validator.isAlpha(name) && validator.isEmail(email);
 
-    redirect(res, 302, data['redirect_to']);
+    if (formIsValid) {
+        console.log('uno');
+        post(form_destination, { name, email, message });
+    } else {
+        console.log('dos');
+        redirect(res, 302, redirect_to);
+    }
 }
 
 module.exports = cors(handler);
