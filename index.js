1bb21133eee3ab99b8c48ed24132f30934c15b41
Jarrod Thibodeau <thibodeaujz@gmail.com>
2019-04-08 21:04:05 -0400

    son of a bitch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { send, createError, run, json } = require('micro');
 const cors = require('micro-cors')();
 
 const handler = async (req, res) => {
+    console.log(req);
     const { myPostData } = await json(req);
 
     console.log('the body: ', myPostData);
