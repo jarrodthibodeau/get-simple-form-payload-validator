585961bf9504e4e37952ad570384aed27dbb9cfa
Jarrod Thibodeau <thibodeaujz@gmail.com>
2019-04-08 21:13:48 -0400

    yayyyyy

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-const { send, createError, run, text } = require('micro');
 const cors = require('micro-cors')();
+const parse = require('urlencoded-body-parser');
+
 
 const handler = async (req, res) => {
-    console.log(req);
-    let myPostData;
+   const data = await parse(req);
 
-    try {
-        myPostData  = await text(req);
-    } catch (e) {
-        return send(res, 400, 'you suck bitch');
-    }
-
-
-    console.log('the body: ', myPostData);
+    console.log('the body: ', data);
 
     return send(res, 200, 'hihihihi');
 }
