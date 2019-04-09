342e87320e196f1901680bb9e3f1dc064fbb4c3f
Jarrod Thibodeau <thibodeaujz@gmail.com>
2019-04-08 21:17:16 -0400

    micro and axios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cors = require('micro-cors')();
 const parse = require('urlencoded-body-parser');
-
+const { send } = require('micro');
+const { post } = require('axios');
 
 const handler = async (req, res) => {
    const data = await parse(req);
