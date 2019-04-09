93c861e780bff87835222f37c10654f979700544
Jarrod Thibodeau <thibodeaujz@gmail.com>
2019-04-08 20:40:37 -0400

    hungry child

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = (req, res) => {
     console.log('req: ', req);
     console.log('payloadddd: ', req.query);
     console.log('params? :', req.params);
+    console.log('SOMETHIGN!: ', req.body);
 
     res.send(200);
 }
