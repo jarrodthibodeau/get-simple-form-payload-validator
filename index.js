5b17aaf9f9e09a4cb07f97b5399f14b1c398a4c0
Jarrod Thibodeau <thibodeaujz@gmail.com>
2019-04-08 21:23:29 -0400

    idiot

--- index.js
+++ index.js
@@ -7,14 +7,9 @@ const handler = async (req, res) => {
     const data = await parse(req);
 
     let formIsValid = true;
-
     console.log('the body: ', data);
 
-
-
-
-
-    redirect(res, 302, data[redirect_to]);
+    redirect(res, 302, data['redirect_to']);
 }
 
 module.exports = cors(handler);
